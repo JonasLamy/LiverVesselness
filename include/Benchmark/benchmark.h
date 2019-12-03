67ac6aeada6c261f0d0c84215be3dfa9a09e86ad
Jonas Lamy <jonas.lamy@univ-lyon2.fr>
2019-12-03 17:39:21 +0100

    adding patient name to csv file. Allowing several patients records to be in a single csv. Still having to change images names to avoid overwriting
    Former-commit-id: a6feedda2e6006e35a18a8479ca22ed853ba99f6

--- include/Benchmark/benchmark.h
+++ include/Benchmark/benchmark.h
@@ -20,16 +20,20 @@ template<class TImageType, class TGroundTruthImageType, class TMaskImageType>
 class Benchmark
 {
 public:
-    Benchmark(const Json::Value root,std::string inputFileName,std::string csvFileName, typename TGroundTruthImageType::Pointer gtImage, typename TMaskImageType::Pointer maskImage);
-    ~Benchmark();
+    Benchmark(const Json::Value root,std::string inputFileName,std::ofstream &csvFileStream, typename TGroundTruthImageType::Pointer gtImage, typename TMaskImageType::Pointer maskImage);
+    ~Benchmark();   
     void SetDicomInput(){m_inputIsDicom = true;}
     void SetNiftiInput(){m_inputIsDicom = false;}
-    void SetOutputDirectory(const std::string outputDir){m_outputDir = outputDir; }
+    void SetOutputDirectory(const std::string &outputDir){m_outputDir = outputDir; }
+    void SetPatientDirectory(const std::string &patient){m_patient = patient;}
 
     void run();
 private:
     std::string m_inputFileName;
     std::string m_outputDir;
+    std::string m_patient;
+
+    static int m_nbAlgorithms;
     bool m_inputIsDicom;
     Json::Value m_rootNode;
 
@@ -44,6 +48,9 @@ private:
     void launchScript(int algoID,const std::string &commandLine,const std::string &outputName);
     void addResultsToFile(int algoID,int threshold, const Eval<TGroundTruthImageType,TGroundTruthImageType,TMaskImageType>& eval);
 };
+
+template<class TImageType, class TGroundTruthImageType, class TMaskImageType>
+int Benchmark<TImageType,TGroundTruthImageType,TMaskImageType>::m_nbAlgorithms = 0;
 
 #include "benchmark.hxx"
 #endif
