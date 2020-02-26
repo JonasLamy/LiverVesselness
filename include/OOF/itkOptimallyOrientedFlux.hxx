fbd1190239710c7beb34c4ead46151a5e5caf4da
Jonas Lamy <jonas.lamy@univ-lyon2.fr>
2020-02-26 12:20:33 +0100

    validating OOF up to normalization step
    Former-commit-id: 4b0199a84efa1c66ce45c5c634191ff4b277fb21

--- include/OOF/itkOptimallyOrientedFlux.hxx
+++ include/OOF/itkOptimallyOrientedFlux.hxx
@@ -1,8 +1,8 @@
 template<typename TInputImage, typename TOutputImage>
 itk::OptimallyOrientedFlux<TInputImage,TOutputImage>::OptimallyOrientedFlux()
+:m_sigma(1.0),m_normalizationType(1),m_responseType(1)
 {
-
-
+    m_radii.push_back(3);
 }
 /*
 template<typename TInputImage, typename TOutputImage>
@@ -83,7 +83,7 @@ std::vector<itk::Image<float,3>::Pointer> itk::OptimallyOrientedFlux<TInputImage
                 pixelIndexZ[1] = col;
                 pixelIndexZ[2] = depth;
                 //computing value for the slice;
-                value = ( depth - static_cast<int>( p[size[2]] ) ) / static_cast<float>(size[2]) / static_cast<float>(spacing[2]);
+                value = ( depth - static_cast<int>( p[2]*2 ) ) / static_cast<float>(size[2]) / static_cast<float>(spacing[2]);
                 Z->SetPixel(pixelIndexZ, value );
             }
         }
@@ -98,7 +98,7 @@ std::vector<itk::Image<float,3>::Pointer> itk::OptimallyOrientedFlux<TInputImage
                 pixelIndexX[1] = col;
                 pixelIndexX[2] = depth;
                 //computing value for the slice;
-                value = depth / static_cast<float>(size[1]) / static_cast<float>(spacing[1]);
+                value = col / static_cast<float>(size[1]) / static_cast<float>(spacing[1]);
                 X->SetPixel(pixelIndexX, value );
             }
             for(int col=p[1];col<size[1];col++)
@@ -107,13 +107,14 @@ std::vector<itk::Image<float,3>::Pointer> itk::OptimallyOrientedFlux<TInputImage
                 pixelIndexX[1] = col;
                 pixelIndexX[2] = depth;
                 //computing value for the slice;
-                value = ( depth - static_cast<int>( p[size[1]] ) ) / static_cast<float>(size[1]) / static_cast<float>(spacing[1]);
+                value = ( col - static_cast<int>( p[1] )*2 ) / static_cast<float>(size[1]) / static_cast<float>(spacing[1]);
                 X->SetPixel(pixelIndexX, value );
             }
         }
     
     // do it for Y
     for(int col=0; col<size[1];col++)
+    {
         for(int depth=0;depth<size[2];depth++)
         {
             for(int row=0;row<p[0];row++)
@@ -122,19 +123,24 @@ std::vector<itk::Image<float,3>::Pointer> itk::OptimallyOrientedFlux<TInputImage
                 pixelIndexY[1] = col;
                 pixelIndexY[2] = depth;
                 //computing value for the slice;
-                value = depth / static_cast<float>(size[0]) / static_cast<float>(spacing[0]);
+                //varying from 0 to p[0]
+                value = (row ) / static_cast<float>(size[0]) / static_cast<float>(spacing[0]);
                 Y->SetPixel(pixelIndexY, value );
             }
+            
             for(int row=p[0];row<size[0];row++)
             {
                 pixelIndexY[0] = row;
                 pixelIndexY[1] = col;
                 pixelIndexY[2] = depth;
                 //computing value for the slice;
-                value = ( depth - static_cast<int>( p[size[0]] ) ) / static_cast<float>(size[0]) / static_cast<float>(spacing[0]);
+                //varying from -p to -1 
+                value = ( row - static_cast<int>( p[0] )*2 )  / static_cast<float>(size[0]) / static_cast<float>(spacing[0]);
                 Y->SetPixel(pixelIndexY, value );
             }
+            
         }
+    }
 
     std::vector<CoordImageType::Pointer> vect;
     vect.push_back(X);
@@ -196,14 +202,15 @@ void itk::OptimallyOrientedFlux<TInputImage,TOutputImage>::GenerateData()
                 index[0] = i;
                 index[1] = j;
                 index[2] = k;
-                value = sqrtf32( vector[0]->GetPixel(index) * vector[0]->GetPixel(index) +
-                                    vector[1]->GetPixel(index) * vector[1]->GetPixel(index) +
-                                    vector[2]->GetPixel(index) * vector[2]->GetPixel(index)   )
-                                    + 1e-12;
+
+                float x_carre = vector[0]->GetPixel(index) * vector[0]->GetPixel(index); 
+                float y_carre = vector[1]->GetPixel(index) * vector[1]->GetPixel(index); 
+                float z_carre = vector[2]->GetPixel(index) * vector[2]->GetPixel(index); 
+                value = sqrt( x_carre + y_carre + z_carre ) + 1e-12;
                 radius->SetPixel(index,value);
             }   
     // erasing vector of X,Y,Z for low memory consuptiom
-    vector.clear();
+    //vector.clear();
 
     using BesselBufferImageType = itk::Image<FFTOutputPixelType,3>;
     using ComplexMultiplyImageFilterType = itk::MultiplyImageFilter<BesselBufferImageType,BesselBufferImageType>;
@@ -211,10 +218,11 @@ void itk::OptimallyOrientedFlux<TInputImage,TOutputImage>::GenerateData()
     float normalization = 0;
     for(auto &rad : m_radii)
     {
-        normalization = 4.0/3.0*M_PI*(rad*rad*rad)/boost::math::cyl_bessel_j<float,float>(1.5,2*M_PI*rad*1e-12)/std::pow(1e-12,1.5)
-        / (rad*rad) * ( rad / std::pow( sqrtf32( 2*rad*m_sigma - m_sigma*m_sigma ),m_normalizationType ) );
-    
-    
+        /* TODO: find out why 1e-12^(3/2) needs to be turned into 1e12^(3/2) to match Matlab implementation */ 
+        normalization = 4.0/3.0*M_PI*(rad*rad*rad)/boost::math::cyl_bessel_j<float,float>(1.5,2*M_PI*rad*1e-12)/std::pow(1e12,1.5)
+        / (rad*rad) * ( std::pow(rad / sqrt( 2*rad*m_sigma - m_sigma*m_sigma ),m_normalizationType ) );
+        std::cout<<normalization<<std::endl;
+
         auto BesselJBuffer = BesselBufferImageType::New();
         BesselJBuffer->SetRegions( input->GetLargestPossibleRegion() );
         BesselJBuffer->SetSpacing( input->GetSpacing() );
@@ -229,6 +237,7 @@ void itk::OptimallyOrientedFlux<TInputImage,TOutputImage>::GenerateData()
         {
             // radius^2 ./ radius^1.5 <-> (radius * radius) / (radius* sqrt(radius)) <-> sqrt(radius) 
             itBesselJBuffer.Value() =  normalization * exp( (-m_sigma*m_sigma)*2*M_PI*M_PI* sqrt(itRadius.Get()) );
+            //std::cout<<itBesselJBuffer.Get()<<std::endl;
             itBesselJBuffer.Value() = std::complex<float>( sin(2*M_PI*rad*itRadius.Get()) / (2*M_PI*rad*itRadius.Get()) - cos(2*M_PI*rad*itRadius.Get()) ) 
                                     * itBesselJBuffer.Get()* std::complex<float>( (1/M_PI/M_PI/rad/itRadius.Get() ) );
             ++itBesselJBuffer;
@@ -239,6 +248,24 @@ void itk::OptimallyOrientedFlux<TInputImage,TOutputImage>::GenerateData()
         multiplyImageFilter->SetInput1(BesselJBuffer);
         multiplyImageFilter->SetInput2(FFTfilter->GetOutput());
         multiplyImageFilter->Update();
+        
+        for(int row=0; row<1;row++)
+        {   
+            for(int col=0;col<size[1];col++)
+            {
+                for(int depth=0;depth<size[2];depth++)
+                {
+                    index[0] = row;
+                    index[1] = col;
+                    index[2] = depth;
+                    std::cout<<radius->GetPixel(index)<<" ";
+                }
+            
+                std::cout<<std::endl;
+            }
+            std::cout<<std::endl;
+        }
+        std::cout<<"\n";
     }
 
     this->GraftOutput(realFilter->GetOutput());
