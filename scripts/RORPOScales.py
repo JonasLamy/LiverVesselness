687c8bc8ff295301a9d4b81c6f2ec9be1b80bff0
Jonas Lamy <jonas.lamy@univ-lyon2.fr>
2020-04-21 12:56:12 +0200

    removing messy scripts, a clean version will be added later

--- scripts/RORPOScales.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-scaleMin = int( sys.argv[1] )
-factor = float(sys.argv[2] )
-nbScales = int( sys.argv[3] )
-
-for i in range(1,nbScales+1):
-    print("scale:"+str(i),int(scaleMin * factor**i))
